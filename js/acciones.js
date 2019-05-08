4a15d6f44410241993f70bc20e44ff350d174b05
yarfernandez <alanferuizreach@gmail.com>
2019-05-07 19:10:01 -0500

    5

--- js/acciones.js
+++ js/acciones.js
@@ -1,11 +1,11 @@
 //JavaScript Document
 
 $(document).ready(function(e){
-	document.addEventListener("deviceready",function(){
+	document.addEventListener ("deviceready",function(){
 		
 		$('#izquierda').on("swipeleft",function(){
 			navigator .notification. alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});
-		});
+});
 		
 		$('#derecha').on("swiperight",function(){
 			navigator .notification. confirm("¿Que quieres hacer?",function(opt){
