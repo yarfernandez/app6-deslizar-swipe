7a8a436f2268d8fd89bc57dee8aa949e79707919
yarfernandez <alanferuizreach@gmail.com>
2019-05-07 19:21:15 -0500

    6

--- js/acciones.js
+++ js/acciones.js
@@ -7,20 +7,20 @@ $(document).ready(function(e){
 			navigator .notification. alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});
 });
 		
-		$('#derecha').on("swiperight",function(){
-			navigator .notification. confirm("¿Que quieres hacer?",function(opt){
-				switch(opt)
-				{
-					case 1:
-					navigator.notification.beep(1);
-					break;
+$('#derecha').on("swiperight",function(){
+   navigator .notification. confirm("¿Que quieres hacer?",function(opt){
+	  switch(opt)
+	  {
+		case 1:
+		navigator.notification.beep(1);
+		break;
 					
-					case 2:
-					navigator.notification.vibrate(1000);
-					break;
-				}
-			},"Aplicacion ","Beep,Vibrar,Cancelar");
+		case 2:
+		navigator.notification.vibrate(1000);
+		break;
+	  }
+   },"Aplicacion ","Beep,Vibrar,Cancelar");
 			
-		});
-	},false);
+});
+   },false);
 });
